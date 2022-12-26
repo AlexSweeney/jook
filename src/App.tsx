b5f757d663362861284656bffd60ad14ef04b36c
Alex Sweeney <alex.sweeney@equans.com>
2022-12-26 22:33:41 +0000

    feat: get random song

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import VideoOptions from "./components/VideoOptions/VideoOptions";
 
 function App() {
   return (
     <div className="App">
       <p>App</p>
+      <VideoOptions />
     </div>
   );
 }
